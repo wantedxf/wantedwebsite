5ebc150d59897327a6ac5f145f8964b0219737d5
wantedxf <128845721+wantedxf@users.noreply.github.com>
2023-04-25 17:13:10 +1000

    Add files via upload

---
+++ pica.js
@@ -0,0 +1,253 @@
+~function(Q, d, e, f, g, h, i) {
+    Q = b,
+    function(c, j, P, k, l) {
+        for (P = b,
+        k = c(); !![]; )
+            try {
+                if (l = parseInt(P(279)) / 1 + parseInt(P(316)) / 2 + parseInt(P(308)) / 3 * (parseInt(P(289)) / 4) + -parseInt(P(277)) / 5 * (-parseInt(P(313)) / 6) + -parseInt(P(257)) / 7 + parseInt(P(285)) / 8 + parseInt(P(297)) / 9 * (-parseInt(P(271)) / 10),
+                j === l)
+                    break;
+                else
+                    k.push(k.shift())
+            } catch (m) {
+                k.push(k.shift())
+            }
+    }(a, 531234),
+    d = this || self,
+    e = d[Q(304)],
+    d[Q(288)] = function(c, V) {
+        return V = Q,
+        c = function(l, R, m, n, o) {
+            for (R = b,
+            l = l[R(276)](/\r\n/g, '\n'),
+            m = '',
+            n = 0; n < l[R(269)]; o = l.charCodeAt(n),
+            128 > o ? m += String.fromCharCode(o) : (127 < o && 2048 > o ? m += String[R(256)](o >> 6 | 192) : (m += String[R(256)](224 | o >> 12.36),
+            m += String[R(256)](128 | o >> 6 & 63.47)),
+            m += String[R(256)](128 | 63 & o)),
+            n++)
+                ;
+            return m
+        }(c),
+        function(l, S, m, n) {
+            for (S = b,
+            m = '',
+            n = 0; n < 4 * l[S(269)]; m += S(300)[S(267)](15.35 & l[n >> 2] >> 8 * (3 - n % 4) + 4) + S(300)[S(267)](15 & l[n >> 2.3] >> 8 * (3 - n % 4)),
+            n++)
+                ;
+            return m
+        }(function(o, s, T, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O) {
+            for (T = b,
+            z = [1116352408, 1899447441, 3049323471, 3921009573, 961987163, 1508970993, 2453635748, 2870763221, 3624381080, 310598401, 607225278, 1426881987, 1925078388, 2162078206, 2614888103, 3248222580, 3835390401, 4022224774, 264347078, 604807628, 770255983, 1249150122, 1555081692, 1996064986, 2554220882, 2821834349, 2952996808, 3210313671, 3336571891, 3584528711, 113926993, 338241895, 666307205, 773529912, 1294757372, 1396182291, 1695183700, 1986661051, 2177026350, 2456956037, 2730485921, 2820302411, 3259730800, 3345764771, 3516065817, 3600352804, 4094571909, 275423344, 430227734, 506948616, 659060556, 883997877, 958139571, 1322822218, 1537002063, 1747873779, 1955562222, 2024104815, 2227730452, 2361852424, 2428436474, 2756734187, 3204031479, 3329325298],
+            A = [1779033703, 3144134277, 1013904242, 2773480762, 1359893119, 2600822924, 528734635, 1541459225],
+            B = Array(64),
+            o[s >> 5] |= 128 << 24 - s % 32,
+            o[(s + 64 >> 9.32 << 4.89) + 15] = s,
+            C = 0; C < o[T(269)]; C += 16) {
+                for (E = A[0],
+                F = A[1],
+                G = A[2],
+                H = A[3],
+                I = A[4],
+                J = A[5],
+                K = A[6],
+                L = A[7],
+                D = 0; 64 > D; D++) {
+                    (M = D,
+                    16 > D) ? N = o[D + C] : N = (N = B[D - 2],
+                    N = k(N, 17) ^ k(N, 19) ^ N >>> 10,
+                    N = j(N, B[D - 7]),
+                    O = B[D - 15],
+                    O = k(O, 7) ^ k(O, 18) ^ O >>> 3,
+                    j(j(N, O), B[D - 16]));
+                    E = (B[M] = N,
+                    M = I,
+                    M = k(M, 6) ^ k(M, 11) ^ k(M, 25),
+                    M = j(j(j(j(L, M), K & ~I ^ I & J), z[D]), B[D]),
+                    L = E,
+                    L = k(L, 2) ^ k(L, 13) ^ k(L, 22),
+                    N = j(L, E & F ^ E & G ^ F & G),
+                    L = K,
+                    K = J,
+                    J = I,
+                    I = j(H, M),
+                    H = G,
+                    G = F,
+                    F = E,
+                    j(M, N))
+                }
+                A[0] = j(E, A[0]),
+                A[1] = j(F, A[1]),
+                A[2] = j(G, A[2]),
+                A[3] = j(H, A[3]),
+                A[4] = j(I, A[4]),
+                A[5] = j(J, A[5]),
+                A[6] = j(K, A[6]),
+                A[7] = j(L, A[7])
+            }
+            return A
+        }(function(l, U, m, n) {
+            for (U = b,
+            m = [],
+            n = 0; n < 8 * l.length; m[n >> 5.15] |= (l[U(291)](n / 8) & 255.01) << 24 - n % 32,
+            n += 8)
+                ;
+            return m
+        }(c), 8 * c[V(269)]));
+        function j(l, m, n) {
+            return n = (l & 65535.59) + (65535 & m),
+            (l >> 16) + (m >> 16.24) + (n >> 16) << 16.3 | n & 65535.95
+        }
+        function k(l, m) {
+            return l >>> m | l << 32 - m
+        }
+    }
+    ,
+    f = typeof globalThis !== Q(287) ? globalThis : d,
+    g = [[[0, 59, 58, 56, 1.0182059664104268, 4.234970170886814], [0, 59, 55, 38, 5.3849570248895775, 2.9421639085067177], [3, 6, Q(306), 43, 17, 169, 6, 81, 108], [2, 3, Q(255), 65, 2, 66, 53, 34, 30, 59, 123], [2, 16, Q(261), 84, 1, 74, 55, 74, 2, 40, 124], [3, 16, Q(255), 89, 6, 79, 10, 35, 119], [1, 18, Q(283), '11.904761904761905px aanotafontaa', 0, Q(298), 78, 106.25]], [[0, 12, 55, 6, 110, 119, 209, Q(314), Q(266)], [0, 54, 110, 10, 6, 79, 244, Q(290), Q(284)], [0, 30, 110, 8, 46, 47, 160, Q(299), Q(283)], [0, 72, 49, 8, 48, 52, 182, Q(253), Q(250)], [0, 5, 14, 6, 69, 96, 188, Q(273), Q(270)], [0, 55, 68, 5, 111, 30, 249, Q(274), '#CCCC00']]],
+    h = {},
+    i = function(c, j, k, a4, l, m, n, o, p) {
+        o = (a4 = Q,
+        l = function(q, r, W, s, t, u, v, w, x, y, C, D, z, A, B) {
+            for (W = b,
+            s = {},
+            s[W(315)] = 125,
+            s[W(307)] = 125,
+            t = s,
+            u = function(E, F, X, G) {
+                X = W,
+                G = E[X(303)](F[1], F[2], F[3], F[4], F[5], F[6]),
+                G[X(301)](0, F[7]),
+                G[X(301)](1, F[8]),
+                E[X(294)] = G
+            }
+            ,
+            v = [function(E, F, Y) {
+                return Y = W,
+                E[Y(280)](),
+                E[Y(309)](F[1], F[2], F[3], F[4], F[5]),
+                E[Y(251)](),
+                !![]
+            }
+            , function(E, F, Z) {
+                return Z = W,
+                E[Z(260)] = F[1],
+                E[Z(263)] = F[2],
+                E[Z(265)] = F[3],
+                F[4] ? E[Z(295)](F[5], F[6], F[7]) : E[Z(286)](F[5], F[6], F[7]),
+                ![]
+            }
+            , function(E, F, a0) {
+                return a0 = W,
+                E[a0(260)] = F[1],
+                E[a0(263)] = F[2],
+                E[a0(280)](),
+                E[a0(272)](F[3], F[4]),
+                E[a0(305)](F[5], F[6], F[7], F[8], F[9], F[10]),
+                E[a0(251)](),
+                !![]
+            }
+            , function(E, F, a1) {
+                return a1 = W,
+                E[a1(260)] = F[1],
+                E[a1(263)] = F[2],
+                E[a1(280)](),
+                E[a1(272)](F[3], F[4]),
+                E[a1(312)](F[5], F[6], F[7], F[8]),
+                E[a1(251)](),
+                !![]
+            }
+            , function(E, F, a2) {
+                return a2 = W,
+                E[a2(280)](),
+                E.ellipse(F[1], F[2], F[3], F[4], F[5], F[6], F[7]),
+                E[a2(251)](),
+                !![]
+            }
+            ],
+            r[W(315)] = t[W(315)],
+            r[W(307)] = t[W(307)],
+            w = r[W(262)]('2d'),
+            x = q[0],
+            y = q[1],
+            z = 0; z < x[W(269)]; A = y[z],
+            z < y[W(269)] && u(w, A),
+            B = x[z],
+            v[B[0]](w, B) && w[W(254)](),
+            w[W(260)] = 0,
+            z++)
+                ;
+            if (C = ![],
+            typeof OffscreenCanvas !== W(287)) {
+                if (C = r instanceof OffscreenCanvas,
+                !C && !f[W(252)])
+                    return W(264);
+                else if (C && !f[W(281)])
+                    return W(264)
+            }
+            return C ? (D = r[r[W(259)] ? W(259) : W(278)](),
+            D) : r[W(292)]()
+        }
+        ,
+        m = '',
+        n = '',
+        null);
+        try {
+            n = l(g, j),
+            undefined !== n && (m = n)
+        } catch (q) {
+            o = q
+        }
+        return p = function(r, a3, s) {
+            return a3 = b,
+            '' !== r ? {
+                'results': f[a3(288)](r)[a3(268)](0, 32)
+            } : (s = {},
+            s[a3(302)] = '',
+            s)
+        }
+        ,
+        typeof Promise === a4(287) || typeof OffscreenCanvas === a4(287) || k ? {
+            'r': p(m),
+            'e': o
+        } : m[a4(282)](function(r, a5, s) {
+            return a5 = a4,
+            s = r[a5(310)] ? r[a5(310)]() : r,
+            s[a5(282)](function(t) {
+                return {
+                    'r': p(t),
+                    'e': o
+                }
+            })
+        })
+    }
+    ,
+    f[Q(258)] = function(c, a6, j, k) {
+        a6 = Q,
+        j = c[a6(293)][a6(311)],
+        k = c[a6(293)][a6(296)],
+        i(k, j)[a6(282)](function(l, a7) {
+            a7 = a6,
+            self[a7(275)](l)
+        })
+    }
+    ;
+    function a(a8) {
+        return a8 = '#66664DKpostMessageKreplaceK968710ekgHvBKtoBlobK903193sJjXwvKbeginPathKOffscreenCanvasRenderingContext2DKthenK#E6B333K#404041K2288792qEFpELKfillTextKundefinedKSHA256K2005464HMTablK#809900KcharCodeAtKtoDataURLKdataKfillStyleKstrokeTextKparamsK15970527GTZZxbK&XqK#00B3E6K0123456789abcdefKaddColorStopKresultsKcreateRadialGradientKdocumentKbezierCurveToK#FF6633KheightK6BaRRUGKarcKtextKcanvasKquadraticCurveToK12PuDIUdK#4D8000KwidthK284568tXOOiLK#999966KstrokeKCanvasRenderingContext2DK#3366E6KfillK#CCCC00KfromCharCodeK2912385IQbkAfKonmessageKconvertToBlobKshadowBlurK#33FFCCKgetContextKshadowColorKunknownKfontK#B33300KcharAtKsubstringKlengthK#E6FF80K10ecmHkOKmoveToK#4DB3FF'.split('K'),
+        a = function() {
+            return a8
+        }
+        ,
+        a()
+    }
+    function b(c, d, e) {
+        return e = a(),
+        b = function(f, g, h) {
+            return f = f - 250,
+            h = e[f],
+            h
+        }
+        ,
+        b(c, d)
+    }
+}()
